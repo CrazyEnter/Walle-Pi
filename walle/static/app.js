e9f6c05270a9dc741168e0beb6d5c47f01c02969
Feagle <Z0006@sinceme.com>
2018-03-15 17:17:51 +0800

    walle controller view

--- walle/static/app.js
+++ walle/static/app.js
@@ -1,21 +1,71 @@
-var socket = io.connect('http://' + document.domain + ':' + location.port);
-socket.on('connect', function() {
-    // we emit a connected message to let knwo the client that we are connected.
-    socket.emit('client_connected', {data: 'New client!'});
+console.log = function(text){
+    $(".console .text").prepend("<p>" + text + "</p>");
+};
+var ws = null;
+var connect = function(url){
+    if (ws !== null)
+        return console.log('already connected');
+
+    ws = new WebSocket(url);
+
+    ws.onopen = function () {
+        console.log('connected');
+    };
+
+    ws.onmessage = function (e) {
+        var recv = e.data;
+        console.log("ws:"+recv);
+    }
+
+    ws.onerror = function (error) {
+        ws = null;
+        console.log('onerror:'+JSON.stringify(error));
+    };
+
+    ws.onclose = function (e) {
+        ws = null;
+        console.log('websocket closed');
+    };
+};
+
+$("#move-up").on("touchstart",function(){
+    if(ws == null)
+        return false;
+    ws.send("up");
+});
+$("#move-up").on("touchend",function(){
+    if(ws == null)
+        return false;
+    ws.send("stop");
+});
+$("#move-right").on("touchstart",function(){
+    if(ws == null)
+        return false;
+    ws.send("right");
+});
+$("#move-right").on("touchend",function(){
+    if(ws == null)
+        return false;
+    ws.send("right");
+});
+$("#move-left").on("touchstart",function(){
+    if(ws == null)
+        return false;
+    ws.send("left");
+});
+$("#move-left").on("touchend",function(){
+    if(ws == null)
+        return false;
+    ws.send("left");
+});
+$("#move-down").on("touchstart",function(){
+    if(ws == null)
+        return false;
+    ws.send("down");
+});
+$("#move-down").on("touchend",function(){
+    if(ws == null)
+        return false;
+    ws.send("down");
 });
 
-socket.on('message', function (data) {
-    console.log('message form backend ' + data);
-});
-
-socket.on('heartbeat', function (data) {
-    console.log('Heartbeating from backend: ' + data);
-});
-
-function heartbeat() {
-    socket.emit('heartbeat', '{"message": "heartbeat"}');
-}
-
-function steering_wheel() {
-    socket.emit('steering_wheel', {data: 'Operating stream from client'});
-}
